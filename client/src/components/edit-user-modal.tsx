90f5679ef5c3bd9cd4996f639b560830cebd662a
Andres Sanabria <andrs.dev@gmail.com>
2020-11-25 11:12:11 -0600

    added timeout for address on change

--- client/src/components/edit-user-modal.tsx
+++ client/src/components/edit-user-modal.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './edit-user-modal.module.css';
 import User from '../models/user.model';
 import Modal from './modal';
 import GoogleMapReact from 'google-map-react';
-import MapsService from 'services/maps.service';
+// import MapsService from 'services/maps.service';
 import PrimaryButton from './primary-button';
 import SecondaryButton from './secondary-button';
 import Marker from './marker';
 
-const mapsService = new MapsService(process.env.REACT_APP_MAPS_API_KEY!)
+// const mapsService = new MapsService(process.env.REACT_APP_MAPS_API_KEY!)
 
 
 interface Props {
@@ -19,6 +19,26 @@ interface Props {
 }
 
 function EditUserModal({ isOpen, user, onSave, onClose } : Props ) {
+
+  let typingTimer: any;
+  let doneTypingInterval = 3000;
+
+  const [coordinates, setCoordinates] = useState({
+    lat: 14.63,
+    lng: -90.50
+  })
+
+  function doneTyping () {
+    console.log("should update pin")
+  }
+
+  const handleLocationChange = (event: React.ChangeEvent<HTMLInputElement>) => { 
+    clearTimeout(typingTimer);
+    if (event.target.value) {
+      typingTimer = setTimeout(doneTyping, doneTypingInterval);
+    }
+  }
+
   return (    
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className={styles.modalContent}>
@@ -38,7 +58,7 @@ function EditUserModal({ isOpen, user, onSave, onClose } : Props ) {
               }}
               defaultZoom={14}
             >
-              <Marker lat={14.63} lng={-90.50} />
+              <Marker lat={coordinates.lat} lng={coordinates.lng} />
             </GoogleMapReact>
           </div>
           <div>
@@ -46,7 +66,7 @@ function EditUserModal({ isOpen, user, onSave, onClose } : Props ) {
               <p><strong>Name</strong></p>
               <input name="name" placeholder="Name" value={user.name}/ >
               <p><strong>Location</strong></p>
-              <input name="location" placeholder="Location"/>
+              <input name="location" placeholder="Location" onChange={handleLocationChange}/>
               <p><strong>Description</strong></p>
               <input name="description" placeholder="Description"/>
             </form>
